25ac76e16c23539e616530d1ec318644c4b75675
Justin Schoeff <jschoeff81690@gmail.com>
2015-02-08 19:20:41 -0500

    Moved reading params to TestClass.java

--- src/sdes/main/SDES.java
+++ src/sdes/main/SDES.java
@@ -1,14 +1,6 @@
 package sdes.main;
 
 import java.util.BitSet;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.math.BigInteger;
-import java.util.Arrays;
-
-import sdes.functions.BinaryRoundKeyGenerator;
 
 public class SDES {
 
@@ -31,77 +23,6 @@ public class SDES {
 	private boolean verbose;
 	private BitSet key;
 	private BitSet plainText;
-	public static File file;
-	
-
-    	public static void main(String[] args) throws IOException{
-        
-    		//String filename = args[0];
-    		String filename = "params.txt"; //default params.txt, comment this out and uncomment line above.  Just using this for easy testing
-    		String string;
-    		String[] parameters = new String[6]; 
-    		file = new File(filename);
-
-        	FileReader fr = new FileReader(file.getAbsolutePath());
-        	BufferedReader br = new BufferedReader(fr);
-
-        	//read in first 5 params
-        	for(int i=0; i<5; i++){ //assumes params.txt is in format of int (tab) comments, with the int being less than 3 digits long
-        		string = br.readLine();
-        		parameters[i] = string.substring(0, 2);
-        		//System.out.println(parameters[i]);
-        	}
-        
-/*		//set params
-        	setBlockSize(Integer.parseInt(parameters[0]));
-        	setKeySize(Integer.parseInt(parameters[1]));
-        	setEffectiveKeySize(Integer.parseInt(parameters[2]));
-        	setRoundKeySize(Integer.parseInt(parameters[3]));
-        	setNumberofRounds(Integer.parseInt(parameters[4]));
-        
-*/
-        
-        	//read in PC-1
-        	string = br.readLine();
-        	String[] fullpc1 = string.split(" "); //only need first 10, rest is comments
-        	String[] perc1 = new String[10];
-        	int[] permc1 = new int[10];
-        	String[] fix = fullpc1[9].split("/");
-        	fullpc1[9] = fix[0];
-        
-        	for (int i=0; i<10; i++){
-        		perc1[i] = fullpc1[i];
-        		//permc1[i] = Integer.parseInt(perc1[i]);
-        	}
-        	//System.out.println(Arrays.toString(permc1));
-        
-        
-        	//read in PC-2
-        	string = br.readLine();
-        	String[] fullpc2 = string.split(" ");
-        	String[] perc2 = new String[8];
-        	int[] permc2 = new int[8];
-        	for (int i=0; i<8; i++){
-        		perc2[i] = fullpc2[i];
-	
-        	}
-        	//System.out.println(Arrays.toString(perc2));
-        
-        	//set pc1 and pc2, NEED TO CONVER THE STRING ARRAYS TO HEX FIRST
-        	for (int i=0; i<9; i++){
-        	//setPc1((perc1));
-        
-        	}
-        	for (int i=0; i<8; i++){
-        
-        	}
-        
-        	br.close();
-    	}
-
-
-
-
 	
 	public void encrypt() {
 		//maybe confirm that all variables are correct?
--- src/sdes/main/TestClass.java
+++ src/sdes/main/TestClass.java
@@ -1,7 +1,9 @@
 package sdes.main;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.BitSet;
 
-import sdes.functions.*;
 
 
 
@@ -12,6 +14,26 @@ public class TestClass {
 	 */
 	public static void main(String[] args) {
 
+		//values from command line args
+		boolean verbose = true;
+		String paramsFile = "params.txt";
+		
+		//commandLine args here?
+		
+		
+		SDES des         =  new SDES();	
+		
+		readParams(paramsFile, des, verbose);
+		
+		des.encrypt();
+	}
+	
+	public static void readParams(String fileName, SDES des, boolean verbose) {
+
+		//String filename = args[0];
+		String filename = "params.txt"; //default params.txt, comment this out and uncomment line above.  Just using this for easy testing
+		String string;
+		String[] parameters = new String[6];
 		int PC1[]        = {3,5,2,7,4,10,1,9,8,6};
 		int IP[]         = {2, 6, 3, 1, 4, 8, 5, 7};
 		int inverse_IP[] = {4, 1, 3, 5, 7, 2, 8, 6};
@@ -23,8 +45,73 @@ public class TestClass {
 		int sBoxes[][][] = {sbox0,sbox1};
 		int rowChoice[]	 = {1,4};
 		int colChoice[]	 = {2,3};
-		SDES des         =  new SDES();	
+
+
 		
+		try {
+			File file = new File(filename);
+
+	    	FileReader fr = new FileReader(file.getAbsolutePath());
+	    	BufferedReader br = new BufferedReader(fr);
+
+	    	//read in first 5 params
+	    	for(int i=0; i<5; i++){ //assumes params.txt is in format of int (tab) comments, with the int being less than 3 digits long
+	    		string = br.readLine();
+	    		parameters[i] = string.substring(0, 2);
+	    		//System.out.println(parameters[i]);
+	    	}
+	    
+	/*		//set params
+	    	des.setBlockSize(Integer.parseInt(parameters[0]));
+	    	des.setKeySize(Integer.parseInt(parameters[1]));
+	    	des.setEffectiveKeySize(Integer.parseInt(parameters[2]));
+	    	des.setRoundKeySize(Integer.parseInt(parameters[3]));
+	    	des.setNumberofRounds(Integer.parseInt(parameters[4]));
+	    
+	*/
+	    
+	    	//read in PC-1
+	    	string = br.readLine();
+	    	String[] fullpc1 = string.split(" "); //only need first 10, rest is comments
+	    	String[] perc1 = new String[10];
+	    	int[] permc1 = new int[10];
+	    	String[] fix = fullpc1[9].split("/");
+	    	fullpc1[9] = fix[0];
+	    
+	    	for (int i=0; i<10; i++){
+	    		perc1[i] = fullpc1[i];
+	    		//permc1[i] = Integer.parseInt(perc1[i]);
+	    	}
+	    	//System.out.println(Arrays.toString(permc1));
+	    
+	    
+	    	//read in PC-2
+	    	string = br.readLine();
+	    	String[] fullpc2 = string.split(" ");
+	    	String[] perc2 = new String[8];
+	    	int[] permc2 = new int[8];
+	    	for (int i=0; i<8; i++){
+	    		perc2[i] = fullpc2[i];
+
+	    	}
+	    	//System.out.println(Arrays.toString(perc2));
+	    
+	    	//set pc1 and pc2, NEED TO CONVER THE STRING ARRAYS TO HEX FIRST
+	    	for (int i=0; i<9; i++){
+	    	//setPc1((perc1));
+	    
+	    	}
+	    	for (int i=0; i<8; i++){
+	    
+	    	}
+	    
+	    	br.close();
+		}
+		catch(Exception e) {
+			System.out.println("There was an error reading params from: "+fileName);
+			System.out.println("Error: "+e.getMessage());
+		}
+
 		des.setRowChoice(rowChoice);
 		des.setColChoice(colChoice);
 		des.setSBoxes(sBoxes);
@@ -40,12 +127,11 @@ public class TestClass {
 		des.setNumberOfRounds(2);
 		des.setBlockSize(8);
 		des.setRotationSchedule(new int[]{1,2});
-		des.setVerbose(true);
+		des.setVerbose(verbose);
 		des.setKey("0110101100");
 		des.setPlainText("10100101");
-		des.encrypt();
+		
 	}
-
 	/* 
 	Converts Binary String to BitSet
 	*/
